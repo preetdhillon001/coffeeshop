df154f9d7e185b7d1378f13c449c392b64376649
preetdhillon001 <129516491+preetdhillon001@users.noreply.github.com>
2024-07-04 12:33:04 -0400

    Add files via upload

---
+++ OrderDetailRepository.java
@@ -0,0 +1,7 @@
+package com.example.coffeeshop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+    List<OrderDetail> findByOrderId(Long orderId);
+}
